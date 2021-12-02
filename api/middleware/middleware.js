3f177fd8812a94fe5e9dc8b070e228b77bf422d6
Desiree Garcia <57469534+Sug4rSku11@users.noreply.github.com>
2021-12-01 17:25:05 -0700

    completed logger

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,26 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timestamp= new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
+  console.log(`[${timestamp}] ${method} to ${url}`)
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validateUserId middleware')
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validateUser middleware')
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validatePost middleware')
 }
 
 // do not forget to expose these functions to other modules
--- api/server.js
+++ api/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const { logger } = require('./middleware/middleware')
 const server = express();
 
 server.use(express.json());
 
 // const usersRouter = require('./users/users-router');
 
-//server.use('/api/users', usersRouter);
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
